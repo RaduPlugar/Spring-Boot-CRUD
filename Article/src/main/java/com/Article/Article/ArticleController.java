48607631c8c9dd79f1b5511ca68a8367947d34f4
RaduPlugar <55532873+RaduPlugar@users.noreply.github.com>
2020-08-27 15:04:37 +0300

    Add files via upload

--- Article/src/main/java/com/Article/Article/ArticleController.java
+++ Article/src/main/java/com/Article/Article/ArticleController.java
@@ -22,19 +22,20 @@ public class ArticleController {
         return articleService.getArticle(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST,value = "/articles")
+    @PostMapping("/articles")
     public void addArticle(@RequestBody Article article){
         articleService.addArticle(article);
     }
 
-    @RequestMapping (method = RequestMethod.PUT,value = "/articles/{id}")
+    @PutMapping("/articles/{id}")
     public  void updateArticles(@PathVariable String id,@RequestBody Article article){
         articleService.updateArticle(article,id);
     }
-    @RequestMapping(method = RequestMethod.DELETE,value = "/articles/{id}")
+    @DeleteMapping("/articles/{id}")
     public void deleteArticles(@PathVariable String id){
         articleService.deleteArticle(id);
     }
+
 
 
 }
